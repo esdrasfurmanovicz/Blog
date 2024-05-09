097f2c967e04f4909a2be9ad82818b25be406084
Esdras Furmanovicz <128192976+esdrasfurmanovicz@users.noreply.github.com>
2024-05-09 17:03:28 -0300

    commit

--- login.php
+++ login.php
@@ -15,6 +15,14 @@
     <link rel="stylesheet" href="style/login.css">
 </head>
 <body>
+    <div class="cadDiv">
+        <div class="cadastro">
+            <span id="close">X</span>
+            <form action="usuario_novo_post" method="post">
+                
+            </form>
+        </div>
+    </div>
     <main>
         <div class="container">
             <div class="areaLogin">
@@ -22,15 +30,32 @@
                     <h1>Bem vindo ao Neriety</h1>
                     <form action="logar.php" method="post">
                         <div class="input-group">
-                            <label for="email">E-mail: </label>
-                            <input type="email" id="email" name="email" required>
+                            <input type="email" id="email" name="email" required placeholder="Email">
                         </div>
                         <div class="input-group">
-                            <label for="senha">Senha: </label>
-                            <input type="text" id="senha" name="senha" required>
+                            <input type="text" id="senha" name="senha" required placeholder="Senha">
                         </div>
                         <button type="submit" class="btn">Entrar</button>
-                        <p class="textCad">Ainda não tem uma conta? <strong class="strCad">Cadastre-se</strong></p>
+                        <div class="separador">
+                            <hr>
+                            <span>Ou</span>
+                            <hr>
+                        </div>
+                        <div class="google-login-button">
+                            <svg stroke="currentColor" fill="currentColor" stroke-width="0" version="1.1" x="0px" y="0px" class="google-icon" viewBox="0 0 48 48" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
+                            <path fill="#FFC107" d="M43.611,20.083H42V20H24v8h11.303c-1.649,4.657-6.08,8-11.303,8c-6.627,0-12-5.373-12-12
+                    c0-6.627,5.373-12,12-12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C12.955,4,4,12.955,4,24
+                    c0,11.045,8.955,20,20,20c11.045,0,20-8.955,20-20C44,22.659,43.862,21.35,43.611,20.083z"></path>
+                            <path fill="#FF3D00" d="M6.306,14.691l6.571,4.819C14.655,15.108,18.961,12,24,12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657
+                    C34.046,6.053,29.268,4,24,4C16.318,4,9.656,8.337,6.306,14.691z"></path>
+                            <path fill="#4CAF50" d="M24,44c5.166,0,9.86-1.977,13.409-5.192l-6.19-5.238C29.211,35.091,26.715,36,24,36
+                    c-5.202,0-9.619-3.317-11.283-7.946l-6.522,5.025C9.505,39.556,16.227,44,24,44z"></path>
+                            <path fill="#1976D2" d="M43.611,20.083H42V20H24v8h11.303c-0.792,2.237-2.231,4.166-4.087,5.571
+                    c0.001-0.001,0.002-0.001,0.003-0.002l6.19,5.238C36.971,39.205,44,34,44,24C44,22.659,43.862,21.35,43.611,20.083z"></path>
+                            </svg>
+                            <span>Logar com o Google</span>
+                        </div>
+                        <p class="textCad">Ainda não tem uma conta? <strong class="strCad" onclick="cadastro()">Cadastre-se</strong></p>
                     </form>
                 </div>
             </div>
@@ -39,5 +64,15 @@
             </div>
         </div>
     </main>
+    <script>
+        function cadastro(){
+            const cadDiv = document.querySelector(".cadDiv")
+            const close = document.querySelector("#close")
+            cadDiv.style.display = "flex"
+            close.onclick = () => {
+                cadDiv.style.display = "none"
+            }
+        }
+    </script>
 </body>
 </html>
